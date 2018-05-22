41ce9070e37564c0aece570650502a9275f21435
Nihar <nihar.dongara@copart.com>
2018-05-22 14:46:59 -0500

    Added React native 0.34.1 method implementations

--- android/src/main/java/com/calendarevents/CalendarEventsPackage.java
+++ android/src/main/java/com/calendarevents/CalendarEventsPackage.java
@@ -4,6 +4,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.bridge.JavaScriptModule;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -16,6 +17,11 @@ public class CalendarEventsPackage implements ReactPackage {
         return Collections.emptyList();
     }
 
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+    
     @Override
     public List<NativeModule> createNativeModules(
             ReactApplicationContext reactContext) {
